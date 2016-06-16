d823aaaac6446d252245b0b8e13552e4962e9b91
Lesmed Gutiérrez <lesmed.gutierrez@clearcorp.co.cr>
2016-06-16 15:49:37 -0600

    [ADD]-budget: Iniciating the separation to new modules

--- cash_budget/models/__init__.py
+++ cash_budget/models/__init__.py
@@ -2,5 +2,8 @@
 # © 2016 ClearCorp
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
-import models
-import wizard
+import budget
+import budget_move
+import budget_move_line
+import budget_program
+import budget_program_line
--- cash_budget_account/models/__init__.py
+++ cash_budget_account/models/__init__.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-# © 2016 ClearCorp
+# © <YEAR(S)> ClearCorp
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 import account_move
@@ -8,14 +8,3 @@ import account_invoice
 import account_move_line
 import account_move_line_distribution
 import account_period
-import budget
-import budget_move
-import budget_move_line
-import budget_program
-import budget_program_line
-import hr_expense
-import hr_payroll
-import purchase
-import res_partner
-import sale
-import stock
