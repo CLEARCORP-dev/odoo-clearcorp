15e5a50d57dd70ba1f4013b9bb6eaa14882c6f1d
Lesmed Gutierrez <lesmed.gutierrez@clearcorp.co.cr>
2016-09-29 08:33:59 -0600

    [FIX] - clearcorp_account_setup: Fixed sumary of module.

--- clearcorp_account_setup/__openerp__.py
+++ clearcorp_account_setup/__openerp__.py
@@ -4,7 +4,7 @@
 
 {
     'name': 'ClearCorp Account Configuration',
-    'summary': 'App Account addons ClearCorp',
+    'summary': 'App to install Account packages ClearCorp',
     'version': '9.0.1.0',
     'category': 'ClearCorp Modules',
     'website': 'http://clearcorp.cr',
@@ -15,14 +15,13 @@
     'installable': True,
     'auto_install': False,
     'depends': [
-        'base_setup',
+        'clearcorp_base_setup',
         'account_account_prefix',
         'account_invoice_discount',
         'account_invoice_report',
         'account_journal_extended_code',
         'account_move_rate',
         'account_move_rate_calculated',
-        
     ],
     'data': [
         'views/res_config_view.xml',
